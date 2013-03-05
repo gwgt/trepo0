f64efb927083527b631d83588f126aa0f4ea0229
gabri3la <vasilik_a@yahoo.com>
2013-03-05 16:24:25 +0200

    first attempt

---
+++ quiz.c
@@ -0,0 +1,13 @@
+// PROJECT ON COMPUTER QUIZ
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+	printf("\n====================\n");
+	printf("\nHELLO JENKINS !");
+	printf("\n====================\n");
+	exit(0);
+}
+
+
+
