7f2325934796862fd5568e3c008d2553650c0823
gabri3la <vasilik_a@yahoo.com>
2013-03-05 14:02:04 +0200

    added exit

--- quiz.c
+++ quiz.c
@@ -1,11 +1,12 @@
 // PROJECT ON COMPUTER QUIZ
 #include<stdio.h>
-
+#include<stdlib.h>
 void main()
 {
 	printf("\n====================\n");
 	printf("\nHELLO JENKINS !");
 	printf("\n====================\n");
+	exit(0);
 }
 
 
