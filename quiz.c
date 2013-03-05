994742b0ae4d1f064eeb7f955e60c9f1856068f0
gabri3la <vasilik_a@yahoo.com>
2013-03-05 16:11:02 +0200

    quiz fourth attemp

--- quiz.c
+++ quiz.c
@@ -5,7 +5,7 @@ void main()
 {
 	printf("\n====================\n");
 	printf("\nHELLO JENKINS !");
-	printf("\nAutomatic build after fourth commit !");
+	printf("\nAutomatic build after fifth commit !");
 	printf("\n====================\n");
 	exit(0);
 }
